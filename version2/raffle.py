6bc30296e72049eca54fe14e503b3dad4841749d
Missy Davies <ms.melissadavies@gmail.com>
2021-03-04 01:08:24 +0000

    Edited version 2 files to separate functions

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,22 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+import customers 
+from random import choice 
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+    
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    all_customers = customers.get_customers_from_file("customers.txt")
+    pick_winner(all_customers)
